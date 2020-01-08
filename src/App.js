3aa660e3f12931162b78be0e6e7d41d6eaf61d5f
unknown <woopyeon02@gmail.com>
2020-01-08 14:10:11 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Nav extends Component {
+  render() {
+    return(
+      <div></div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+      Hello world!
+    </div>
+    );
+  }
+}
+
+export default App;
