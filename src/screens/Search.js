37a489cd6f1dcfcc818a54a4ad7341b76990e5c8
leehyeongrak <leehrak@gmail.com>
2020-01-10 19:11:48 +0900

    markerDisplay, latlngToUtmk

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import SearchList from '../components/SearchList';
 import {Container, Header, Button, Card, Image, Icon, Grid, GridColumn, Segment, Menu, Table} from 'semantic-ui-react'
 import axios from "axios"
-import ReactDOM from "react-dom";
 
 const getResults = async () => {
     try {
@@ -17,7 +16,32 @@ const showResults = async () => {
     
     console.log(results)
 };
-  
+
+//--- 하단은 gis 관련 코드입니다. ---
+function markerDisplay(res, map){
+    res.data.DATA.some((element, idx) => {
+      if(element.xcode && element.ycode){
+        let point = latlngToUtmk(element.ycode, element.xcode)
+        let marker = new window.olleh.maps.overlay.Marker({
+          position: point,
+          caption: '공영주차장',
+          map: map
+        });
+        // marker.setIcon('https://www.gunpopark.or.kr/images/main/about_icon.png');
+        marker['attribute'] = element
+        marker.onEvent('click', function(e) {
+          window.olleh.infoWindowShow(this)
+        });
+      }
+    })
+  }
+
+function latlngToUtmk(x, y){ 
+    let p = window.olleh.maps.UTMK.valueOf(new window.olleh.maps.LatLng(x, y))
+    return p
+}
+//--- 상단은 gis 관련 코드입니다. ---
+
 const extra = (
     <div>
       <Button primary>온라인 전시</Button>
@@ -79,28 +103,47 @@ const CardExample = () => (
 
 class Search extends React.Component {
     state = {
-      searchResultMessage: "...을 검색한 결과입니다."
-
+        searchResultMessage: "...을 검색한 결과입니다.",
+        searchList: "z"
     };
     
+    componentWillMount() {
+        // 서버로부터 받은 데이터를 가지고 setState
+        this.setState({
+            searchResultMessage: "강남역을 검색한 결과입니다.",
+            searchList: "asds"
+        })
+    }
 
-    async componentDidMount(){
+    async componentDidMount() {
         var mapOpts = {
             center: new window.olleh.maps.UTMK(960823.7, 1945435.52),
             zoom: 8,
             mapTypeId: 'ROADMAP'
         };
-
         var map = new window.olleh.maps.Map(document.getElementById("map_div",),
             mapOpts
         );
+
+        axios({
+            method:'get',
+            url : 'https://kt_map.gitlab.io/data/public_parking.json',
+            responseType: 'json',
+          }).then(res => {
+            markerDisplay(res, map);
+          }).catch(err => console.log(err));
+
+ 
+        console.log(this.state.searchResultMessage)
     }
 
     render() {
+        console.log(this.state.searchResultMessage)
         return (
             <div>
                 {SearchLayout(this.state.searchResultMessage)}
             </div>
+            
         );
     }
 }
