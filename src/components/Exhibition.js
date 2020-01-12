00db2240ae010cdd93069d2ec359be2f1e5b91bf
leehyeongrak <leehrak@gmail.com>
2020-01-13 03:35:39 +0900

    Search page css 수정

--- src/components/Exhibition.js
+++ src/components/Exhibition.js
@@ -1,17 +1,54 @@
 import React from "react";
 import PropTypes, { func } from "prop-types";
-import '../css/Exhibition.css'
+// import '../css/Exhibition.css'
+import { Card, Icon, Image, Button } from 'semantic-ui-react'
 
-function Exhibition({id, title, place, address, date, time, price, poster}){
-    return <div className="exhibition">
-        <img src={poster} alt={title} title={title}/>
-        <div className="exhibition_data">
-            <h3 className="exhibition_title">{title}</h3>
-            <h5 className="exhibition_date">{date}</h5>
-            {/* <ul className="exhibition_genres">{genres.map((genre, index) => <li key={index} className="genres_genre">{genre}</li>)}</ul> */}
-            <p className="exhibition_place">{place}</p>
-        </div>
-    </div>
+const numberingWithIndex = (index) => {
+    switch (index) {
+        case 0:
+            return <h1>A</h1>
+            break;
+        case 1:
+            return <h1>B</h1>
+            break;
+        case 2:
+            return <h1>C</h1>
+            break;
+        case 3:
+            return <h1>D</h1>
+            break;
+        case 4:
+            return <h1>E</h1>
+            break;
+        default:
+            return <h1>F</h1>
+            break;
+    }
+}
+
+function Exhibition({id, title, place, address, date, time, price, poster, index}){
+    console.log(index);
+    return (
+        <Card style={{height:'210px'}}>
+            <Card.Content>
+                <Image
+                floated='right'
+                size='tiny'
+                src={poster}
+                alt={title}
+                title={title}
+                />
+                <Card.Header>{numberingWithIndex(index)}</Card.Header>
+                <Card.Meta><strong>{title}</strong></Card.Meta>
+                <Card.Description>
+                {place}
+                </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <Card.Meta>{date}</Card.Meta>
+            </Card.Content>
+        </Card>
+    )
 }
 
 Exhibition.prototype = {
@@ -22,7 +59,8 @@ Exhibition.prototype = {
     date: PropTypes.string.isRequired,
     time: PropTypes.string.isRequired,
     price: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    index: PropTypes.number
     // genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -4,7 +4,7 @@ import { markerDisplay } from '../components/Map';
 import axios from "axios"
 import Exhibition from '../components/Exhibition'
 import '../css/Search.css'
-import Button from '@material-ui/core/Button';
+import { Grid, Image, Button } from 'semantic-ui-react'
 
 const getMap = () => (
   <div>
@@ -58,66 +58,211 @@ class Search extends React.Component {
       this.setState({currentExhibitions: pagedExhibitions, page: page})
     }
 
+    // presentExhibition = (index) => {
+    //   const { currentExhibitions } = this.state
+    //     (currentExhibitions.length <= index)? <div></div> :
+    //     <Exhibition
+    //       key={currentExhibitions[index].exhibit_id}
+    //       id={currentExhibitions[index].exhibit_id}
+    //       title={currentExhibitions[index].title}
+    //       place={currentExhibitions[index].place}
+    //       address={currentExhibitions[index].address}
+    //       date={currentExhibitions[index].date}
+    //       time={currentExhibitions[index].time}
+    //       price={currentExhibitions[index].price}
+    //       poster={currentExhibitions[index].poster}
+    //       index={index}
+    //     />                     
+    // }
+
   render() {
     const { isLoading, exhibitions, page, maxPage, currentExhibitions } = this.state
-    console.log(currentExhibitions);
+    console.log(page);
     return (
-      <div>
-        <div className="searchMessage" style={{
-            width:'100%',
-            height:'20%'
+      <Grid style={{
+        width: '100%',
+        height: '100%',
+        padding: '50px',
+        margin: '0'
+      }}>
+        <Grid.Row style={{
+            height: '15%'
           }}>
-            <h1>keyword에 대한 검색결과 입니다.</h1>
-          </div>
-          <section className="container">
-          {isLoading ? (
-            <div className="loader">
-              <span className="loader_text">Loading...</span>
+          <Grid.Column width={16} style={{
+              height:'100%'
+            }}>
+            {/* 검색키워드 들어갈곳 */}
+            <div className="searchMessage" style={{
+              width:'100%',
+              height:'100%'
+            }}>
+              <h1><strong>keyword</strong>에 대한 검색결과 입니다.</h1>
             </div>
-          ) : (
-            <section className="container">
-              <div className="map_container">
-                    
-                <div id='map_div'>
-                    {getMap()}
-                </div>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row style={{
+            height: '85%'
+          }}>
+          <Grid.Column width={6} style={{
+            height: '100%'
+          }}>
+            {/* 지도들어갈곳 */}
+            <div className="map_container" style={{
+              width: '100%',
+              height: '100%'
+            }}>
+              <div id='map_div'>
+                {getMap()}
               </div>
-              <div className="exhibitions">
-                  {currentExhibitions.map(exhibition => (
+            </div>
+          </Grid.Column>
+          <Grid.Column celled='internally' width={10} style={{
+              height: '100%'
+            }}>
+            <Grid style={{
+              height: '90%'
+            }}>
+              {/* 전시회목록 */}
+              <Grid.Row columns={3} style={{
+                height: '50%'
+              }}>
+                <Grid.Column>
+                  {
+                    (currentExhibitions.length == 0)? <div></div> :
                     <Exhibition
-                      key={exhibition.exhibit_id}
-                      id={exhibition.exhibit_id}
-                      title={exhibition.title}
-                      place={exhibition.place}
-                      address={exhibition.address}
-                      date={exhibition.date}
-                      time={exhibition.time}
-                      price={exhibition.price}
-                      poster={exhibition.poster}
-                      />
-                    ))}
-              </div>
-            </section>
-          )
-        }
-        </section>
-        <div>
-          <Button variant="contained" onClick={() => {
-            if (page>0) {
-              this.nextPaging(exhibitions, page-1);
-            } else {
-              console.log('nothing');
-            }
-          }}>이전</Button>
-          <Button variant="contained" onClick={() => {
-            if (page<maxPage) {
-              this.nextPaging(exhibitions, page+1);
-            } else {
-              this.nextPaging(exhibitions, 0);
-            }
-          }}>다음</Button>
-        </div>
-      </div>
+                      key={currentExhibitions[0].exhibit_id}
+                      id={currentExhibitions[0].exhibit_id}
+                      title={currentExhibitions[0].title}
+                      place={currentExhibitions[0].place}
+                      address={currentExhibitions[0].address}
+                      date={currentExhibitions[0].date}
+                      time={currentExhibitions[0].time}
+                      price={currentExhibitions[0].price}
+                      poster={currentExhibitions[0].poster}
+                      index={0}
+                    />                     
+                  }
+                </Grid.Column>
+                <Grid.Column>
+                  {
+                    (currentExhibitions.length <= 1)? <div></div> :
+                    <Exhibition
+                      key={currentExhibitions[1].exhibit_id}
+                      id={currentExhibitions[1].exhibit_id}
+                      title={currentExhibitions[1].title}
+                      place={currentExhibitions[1].place}
+                      address={currentExhibitions[1].address}
+                      date={currentExhibitions[1].date}
+                      time={currentExhibitions[1].time}
+                      price={currentExhibitions[1].price}
+                      poster={currentExhibitions[1].poster}
+                      index={1}
+                    />                     
+                  }
+                </Grid.Column>
+                <Grid.Column>
+                  {
+                    (currentExhibitions.length <= 2)? <div></div> :
+                    <Exhibition
+                      key={currentExhibitions[2].exhibit_id}
+                      id={currentExhibitions[2].exhibit_id}
+                      title={currentExhibitions[2].title}
+                      place={currentExhibitions[2].place}
+                      address={currentExhibitions[2].address}
+                      date={currentExhibitions[2].date}
+                      time={currentExhibitions[2].time}
+                      price={currentExhibitions[2].price}
+                      poster={currentExhibitions[2].poster}
+                      index={2}
+                    />                     
+                  }
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns={3} style={{
+                height: '50%'
+              }}>
+                <Grid.Column>
+                  {
+                    (currentExhibitions.length <= 3)? <div></div> :
+                    <Exhibition
+                      key={currentExhibitions[3].exhibit_id}
+                      id={currentExhibitions[3].exhibit_id}
+                      title={currentExhibitions[3].title}
+                      place={currentExhibitions[3].place}
+                      address={currentExhibitions[3].address}
+                      date={currentExhibitions[3].date}
+                      time={currentExhibitions[3].time}
+                      price={currentExhibitions[3].price}
+                      poster={currentExhibitions[3].poster}
+                      index={3}
+                    />                     
+                  }
+                </Grid.Column>
+                <Grid.Column>
+                  {
+                    (currentExhibitions.length <= 4)? <div></div> :
+                    <Exhibition
+                      key={currentExhibitions[4].exhibit_id}
+                      id={currentExhibitions[4].exhibit_id}
+                      title={currentExhibitions[4].title}
+                      place={currentExhibitions[4].place}
+                      address={currentExhibitions[4].address}
+                      date={currentExhibitions[4].date}
+                      time={currentExhibitions[4].time}
+                      price={currentExhibitions[4].price}
+                      poster={currentExhibitions[4].poster}
+                      index={4}
+                    />                     
+                  }
+                </Grid.Column>
+                <Grid.Column>
+                  {
+                    (currentExhibitions.length <= 5)? <div></div> :
+                    <Exhibition
+                      key={currentExhibitions[5].exhibit_id}
+                      id={currentExhibitions[5].exhibit_id}
+                      title={currentExhibitions[5].title}
+                      place={currentExhibitions[5].place}
+                      address={currentExhibitions[5].address}
+                      date={currentExhibitions[5].date}
+                      time={currentExhibitions[5].time}
+                      price={currentExhibitions[5].price}
+                      poster={currentExhibitions[5].poster}
+                      index={5}
+                    />                     
+                  }
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+            <Grid style={{
+              // height: '10%'
+            }}>
+              <Grid.Row>
+                <Grid.Row columns={1}>
+                  <Grid.Column>
+                    {/* 버튼들 들어갈곳 */}
+                    <div className="buttons">
+                      <Button size="huge" labelPosition='left' icon='left chevron' content='Prev' onClick={() => {
+                        if (page>0) {
+                          this.nextPaging(exhibitions, page-1);
+                        } 
+                      }}/>
+                      <Button size="huge" labelPosition='right' icon='right chevron' content='Next' onClick={() => {
+                        if (page<maxPage) {
+                          this.nextPaging(exhibitions, page+1);
+                        } else {
+                          this.nextPaging(exhibitions, 0);
+                        }
+                      }}/>
+                    </div>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid.Row>
+            </Grid>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
       )
     }
 }
