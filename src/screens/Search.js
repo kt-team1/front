38cc6fdfe662c5872d6e0a094767d3b9a62bcd07
leehyeongrak <leehrak@gmail.com>
2020-01-12 17:57:41 +0900

    paging

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -14,8 +14,16 @@ const getMap = () => (
 
 const pagingExhibitions = (exhibitions, page) => {
   const pagedExhibitions = [];
-  for (let index = page*6; index < (page+1)*6; index++) {
-    pagedExhibitions.push(exhibitions[index]);
+  const maxPage = parseInt(exhibitions.length/6);
+  if(page==maxPage) {
+    const remainder = exhibitions.length%6;
+    for (let index = page*6; index < page*6+remainder; index++) {
+      pagedExhibitions.push(exhibitions[index]);
+    }
+  } else {
+    for (let index = page*6; index < (page+1)*6; index++) {
+      pagedExhibitions.push(exhibitions[index]);
+    }
   }
   return pagedExhibitions;
 }
@@ -26,7 +34,8 @@ class Search extends React.Component {
       keyword: "...을 검색한 결과입니다.",
       exhibitions: [],
       currentExhibitions: [],
-      page: 0
+      page: 0,
+      maxPage: 0
     };
     
     getExhibitions = async () => {
@@ -36,7 +45,8 @@ class Search extends React.Component {
           }
       } = await axios.get('http://211.254.213.185:5000/searchapi');
       const pagedExhibitions = pagingExhibitions(search, 0);
-      this.setState({exhibitions: search, currentExhibitions:pagedExhibitions, isLoading: false})
+      const maxPage = parseInt(search.length/6);
+      this.setState({exhibitions: search, currentExhibitions:pagedExhibitions, maxPage: maxPage, isLoading: false})
     }
 
     async componentDidMount() {
@@ -49,7 +59,8 @@ class Search extends React.Component {
     }
 
   render() {
-    const { isLoading, exhibitions, page, currentExhibitions } = this.state
+    const { isLoading, exhibitions, page, maxPage, currentExhibitions } = this.state
+    console.log(currentExhibitions);
     return (
       <div>
         <div className="searchMessage" style={{
@@ -92,7 +103,18 @@ class Search extends React.Component {
         </section>
         <div>
           <Button variant="contained" onClick={() => {
-            this.nextPaging(exhibitions, page+1);
+            if (page>0) {
+              this.nextPaging(exhibitions, page-1);
+            } else {
+              console.log('nothing');
+            }
+          }}>이전</Button>
+          <Button variant="contained" onClick={() => {
+            if (page<maxPage) {
+              this.nextPaging(exhibitions, page+1);
+            } else {
+              this.nextPaging(exhibitions, 0);
+            }
           }}>다음</Button>
         </div>
       </div>
