877fee8b2b49523ac87efaed06701d0e017a7523
Dawoon <rhdrlvnd@ajou.ac.kr>
2020-01-13 18:41:14 +0900

    edit index.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,9 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import './css/index.css';
-<<<<<<< HEAD
 import 'semantic-ui-css/semantic.min.css';
-=======
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
-
->>>>>>> 7710cee156bd21ab3c6c80a4caa5049c1fb058d5
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
