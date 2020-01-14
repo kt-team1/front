c592c399eaff6b9decbba0bbfc0ed0c1e9001bc0
unknown <woopyeon02@gmail.com>
2020-01-15 08:57:25 +0900

    final please

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -55,7 +55,7 @@ class SearchBar extends Component {
                 <h4 style={{fontSize: '1.3vw'}}>기가지니와 함께 떠나는 문화 여행</h4>
                 <p style={{fontSize: '1.3vw'}}>-</p>
                 <p style={{fontSize: '1.3vw'}}>"광화문 주변 전시회 보여줘"</p>
-                <p style={{fontSize: '1.3vw'}}>"기가지니 온라인 전시회 보여줘"</p>
+                {/* <p style={{fontSize: '1.3vw'}}>"기가지니 온라인 전시회 보여줘"</p> */}
                 <div className="input_container" style={{position: 'absolute', width: '100%', height: '20%', bottom: '0'}}>
                     <InputForm/>
                 </div>
