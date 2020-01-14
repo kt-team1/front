98a71edda2f107e9d77d1c9cac6a017a5c6f7662
unknown <woopyeon02@gmail.com>
2020-01-14 10:47:32 +0900

    home font update

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -5,7 +5,7 @@ class SearchBar extends Component {
     render() {
         return (
             <div className="search_bar">
-                <h1 style={{fontSize: '2.0vw',}}>기가지니</h1>
+                <h1 style={{fontSize: '2.0vw'}}>기가지니</h1>
                 <h2 style={{fontSize: '1.6vw'}}>인기 있는 전시 검색해줘</h2>
                 <h4 style={{fontSize: '1.3vw'}}>기가지니와 함께 떠나는 문화 여행</h4>
                 <p style={{fontSize: '1.3vw'}}>-</p>
